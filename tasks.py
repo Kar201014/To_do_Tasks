40e31deceb4c5040d3644b0672aa6e1716ba77df
Kar201014 <karenkarapetian2@gmail.com>
2024-11-24 13:58:01 +0400

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,41 @@
+# def add_grqer():
+#     girq_name = input("enter girq name: ")
+#     girq_priority=input("enter girq priority high/medium/low: ")
+#     grqer[girq_name] = girq_priority
+#     print(f'girq {girq_name} added with priority {girq_priority}')  
+    
+    
+# def view_grqer():
+#    if not grqer:
+#        print('no grqer')
+       
+#    else:      
+#         print('current tasks')
+#         for girq, propertyin in grqer.items():
+#             print(f"- {girq} (priority:{propertyin})")   
+            
+            
+            
+# def remove_girq():
+#     if not grqer:
+#       print("no grqer")
+#       return
+#     grqer_to_remove= input("enter the grqer name for the remove")
+#     if grqer_to_remove in grqer:
+#         del grqer[grqer_to_remove]    
+#         print(f'Task {grqer_to_remove} removed')     
+        
+
+
+# while True:
+#     choise = input("1-5")
+#     if choise == "1":
+#        add_grqer()
+#     elif choise == "2":
+#        view_grqer()
+#     elif choise == "3":
+#        remove_girq()
+#     elif choise == "4":
+#         break
+#     else:
+#          print ("sxal tiv")  
